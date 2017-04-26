7f1d87fa8fcb5cf783bd17f2f08eb4a8b94c0d2b
Alan Viverette <alanv@google.com>
2017-04-26 11:01:24 -0400

    Explicitly include android-support-v4
    Test: make checkbuild tests
    Change-Id: Ia62442e72e76fe031242d7ddf509b4055ac81259

--- Android.mk
+++ Android.mk
@@ -22,6 +22,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_STATIC_JAVA_LIBRARIES := android-common
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
+        android-support-v4 \
         android-support-v13
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src) $(call all-renderscript-files-under, src)
