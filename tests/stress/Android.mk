c389099a3cfb941884c5160eb1ca2d821b68feb8
Anton Hansson <hansson@google.com>
2018-02-23 11:51:41 +0000

    packages/apps/Launcher2: Set LOCAL_SDK_VERSION where possible.
    This change sets LOCAL_SDK_VERSION for all packages where
    this is possible without breaking the build, and
    LOCAL_PRIVATE_PLATFORM_APIS := true otherwise.
    
    Setting one of these two will be made required soon, and this
    is a change in preparation for that. Not setting LOCAL_SDK_VERSION
    makes the app implicitly depend on the bootclasspath, which is
    often not required. This change effectively makes depending on
    private apis opt-in rather than opt-out.
    
    Test: make relevant packages
    Bug: 73535841
    Change-Id: Iddfbd884df27cea840cd7679afefdb260ac93840
    Merged-In: Id2fb0111c37a6ebc5c3962cc13db9bff03f81a6f

--- tests/stress/Android.mk
+++ tests/stress/Android.mk
@@ -24,6 +24,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := junit legacy-android-test
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := LauncherRotationStressTest
+LOCAL_PRIVATE_PLATFORM_APIS := true
 
 LOCAL_CERTIFICATE := shared
 
